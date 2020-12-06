1193d9ae44d89474a50994ee93f9d695052db8c3
Vaibhav Acharya <vaibhavacharya111@gmail.com>
2020-12-06 12:23:04 +0530

    Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import Head from "next/head";
+
+import "./../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="A short description of this site."
+        />
+
+        {/* <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" /> */}
+
+        <title>Untitled</title>
+      </Head>
+      <div className="font-sans">
+        <Component { ...pageProps } />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    content: [
+      './pages/**/*.js'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
